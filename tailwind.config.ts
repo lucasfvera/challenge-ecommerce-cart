09c574df0af2f5454d0a17038f607b8d08f397ea
lucasfvera <lucasfvera@hotmail.com>
2025-07-11 23:13:31 -0300

    style: Add typographies to theme
    We need to customize Tailwind's main theme to match our styling.
    
    This commit adds the proper fonts styles to the theme to ensure that
    we can directly use Tailwind utility classes without the need to
    create new text components.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,75 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        "2xl": [
+          "36px",
+          {
+            lineHeight: "40px",
+            letterSpacing: "0.4px",
+            fontWeight: "700",
+          },
+        ],
+        xl: [
+          "24px",
+          {
+            lineHeight: "28px",
+            letterSpacing: "0.4px",
+            fontWeight: "400",
+          },
+        ],
+        xs: [
+          "20px",
+          {
+            lineHeight: "24px",
+            letterSpacing: "0.4px",
+            fontWeight: "400",
+          },
+        ],
+        ag: [
+          "16px",
+          {
+            lineHeight: "20px",
+            letterSpacing: "0px",
+            fontWeight: "400",
+          },
+        ],
+        lg: [
+          "18px",
+          {
+            lineHeight: "24px",
+            letterSpacing: "0.4px",
+            fontWeight: "400",
+          },
+        ],
+        "button-desktop": [
+          "16px",
+          {
+            lineHeight: "16px",
+            letterSpacing: "0.5px",
+            fontWeight: "700",
+          },
+        ],
+        "button-mobile": [
+          "14px",
+          {
+            lineHeight: "16px",
+            letterSpacing: "0.5px",
+            fontWeight: "700",
+          },
+        ],
+        body2: [
+          "14px",
+          {
+            lineHeight: "16px",
+            letterSpacing: "0px",
+            fontWeight: "400",
+          },
+        ],
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
