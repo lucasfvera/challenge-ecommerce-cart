2552fd2992c94b37fccc00ae7d318c499578a949
lucasfvera <lucasfvera@hotmail.com>
2025-07-12 00:17:36 -0300

    feat: Add button component
    We need to have proper buttons to style our pages. For now, we have
    a primary and secondary button designs.
    
    This commit implements the basic styling for the button. We will
    add the interactivity in follow-up commits.

---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+
+interface ButtonProps {
+  children: ReactNode;
+  variant?: "primary" | "secondary" | "tertiary";
+  size?: "sm" | "md";
+}
+
+export const Button = ({
+  variant = "primary",
+  size = "md",
+  children,
+  ...props
+}: ButtonProps) => {
+  const baseClasses = "text-button-mobile md:text-button-desktop rounded-lg";
+
+  const variantClasses = {
+    primary: "text-white bg-[#585660] hover:bg-[#3E3C44] ",
+    secondary: "border border-[#3B3B3B] text-[#3B3B3B] hover:bg-[#EEEEEE]",
+    tertiary: "",
+  };
+
+  const sizeClasses = {
+    sm: "px-6 py-2",
+    md: "px-6 py-4",
+  };
+
+  const classes = [
+    baseClasses,
+    variantClasses[variant],
+    sizeClasses[size],
+  ].join(" ");
+
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
+};
