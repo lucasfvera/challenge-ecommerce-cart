3e09873c06076f00fff7647ab50d28a4918346a0
lucasfvera <lucasfvera@hotmail.com>
2025-07-12 10:16:31 -0300

    feat: Improve Card component
    We need to display different data in the cards for each game to be able
    to show a list of game cards.
    
    This commit adds the functionality to allow showing content in the
    card depending on the data provided.

--- src/components/Molecules/GameCard/GameCard.tsx
+++ src/components/Molecules/GameCard/GameCard.tsx
@@ -1,28 +1,35 @@
 import { Button } from "@/components/Atoms/Button/Button";
+import { Game } from "@/utils/endpoint";
 import Image from "next/image";
 
-interface ItemCardProps {}
+interface GameCardProps {
+  game: Game;
+}
 
-export const ItemCard = ({}: ItemCardProps) => {
+export const GameCard = ({ game }: GameCardProps) => {
+  const { genre, price, name, isNew, image } = game;
+
   return (
     <div className="p-6 rounded-2xl border-[0.5px] border-[#8F8F8F] max-w-[380px] flex flex-col gap-5">
       <div className="relative min-h-[240px] rounded-t-2xl overflow-hidden">
-        <p className="absolute top-3 left-3 rounded px-3 py-2 bg-white z-10 text-tag-desktop">
-          New
-        </p>
+        {isNew && (
+          <p className="absolute top-3 left-3 rounded px-3 py-2 bg-white z-10 text-tag-desktop">
+            New
+          </p>
+        )}
         <Image
           alt=""
-          src={"/game-images/hollowknight.jpeg"}
+          src={image}
           fill
           sizes="(width > 640px) 332px"
           className="object-cover"
         />
       </div>
       <div className="flex flex-col gap-3">
-        <p className="text-ag font-bold text-[#737373]">GENRE</p>
+        <p className="text-ag font-bold text-[#737373]">{genre}</p>
         <div className="flex w-full justify-between text-[#3B3B3B]">
-          <p className="text-lg font-bold text-inherit">Product name</p>
-          <p className="text-xs font-bold text-inherit">$ 119</p>
+          <p className="text-lg font-bold text-inherit">{name}</p>
+          <p className="text-xs font-bold text-inherit">$ {price}</p>
         </div>
       </div>
       <Button variant="secondary">ADD TO CART</Button>
