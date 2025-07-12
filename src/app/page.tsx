38cc18e63c4f9670890f472ef27035afc514d92c
lucasfvera <lucasfvera@hotmail.com>
2025-07-11 23:24:11 -0300

    feat: Add basic page skeleton structure
    We need to start adding styles to the main page. To make it easier to
    implement the main navigation, we can add some basic structure that
    would help us visualize the card list.
    
    This commit adds the bare bones of the main page structure with the
    proper font and sizes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default async function Home() {
-	return <div> main </div>;
+  return (
+    <div className="w-full">
+      <p className="text-2xl">Top Sellers</p>
+      <div className="flex gap-2 justify-end w-full">
+        <p className="text-xs font-bold">Genre</p>
+        <div className="w-px h-[22px] border-r-2 border-[#3B3B3B]" />
+        <p className="text-xs ">All</p>
+      </div>
+    </div>
+  );
 }
