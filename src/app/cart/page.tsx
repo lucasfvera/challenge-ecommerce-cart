9133c963330e59b4093df5ba7f98df2e2d7a1478
lucasfvera <lucasfvera@hotmail.com>
2025-07-15 23:22:46 -0300

    style: Add styling to back link in cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,12 +1,20 @@
 import { CartGamesList } from "@/components/Organisms/CartGamesList/CartGamesList";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function CartPage() {
   return (
     <div className="w-full pb-12">
-      <Link href={"/"}>{"<- Back to Catalog"}</Link>
-      <p className="text-2xl">Your Cart</p>
-      <p className="text-xl">3 items</p>
+      <div className="py-6">
+        <Link href={"/"} className="flex w-fit gap-2">
+          <Image alt="" src={"/arrow-left-icon.svg"} width={24} height={24} />
+          {"Back to Catalog"}
+        </Link>
+      </div>
+      <div className="py-12">
+        <p className="text-2xl">Your Cart</p>
+        <p className="text-xl">3 items</p>
+      </div>
       <div className="flex">
         <CartGamesList />
         {/* <OrderSummaryTable /> */}
