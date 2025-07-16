ecd7040e961e504cce4554afe5eefa11177f66ab
lucasfvera <lucasfvera@hotmail.com>
2025-07-15 22:57:55 -0300

    feat: Add endpoint and action to fetch by id
    We are storing the ids in localstorage and not the whole item to avoid
    duplicating the source of truth for games and ending up with stale
    data.
    The games endpoint is paginated which means that we would need to fetch
    every game to restore the cart only with the ids.
    Since we store the items in the localstorage, we cannot access the ids
    in the server without passing them directly from the client.
    
    This commit creates a way to fetch the games by ids reusing the same
    endpoint that we had.

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -11,29 +11,31 @@ export type GamesResponse = {
 
 export async function GET(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url);
+
+  // If 'ids' param is present, filter by IDs and return only those games
+  const idsParam = searchParams.get("ids");
+  if (idsParam) {
+    const ids = idsParam.split(",").map((id) => id.trim());
+    const games = allGames.filter((game) => ids.includes(game.id));
+    return Response.json({ games });
+  }
+
   const genre = searchParams.get("genre");
   let page = parseInt(searchParams.get("page") ?? "1");
-
   let games = allGames;
-
   if (genre) {
     games = games.filter(
       (game) => game.genre.toLowerCase() === genre.toLowerCase()
     );
   }
-
   if (page < 1 || isNaN(page)) page = 1;
-
   // Mock a delay to simulate a real API
   await delay(2000);
-
   const fromIndex = (page - 1) * ITEMS_PER_PAGE;
   const toIndex = page * ITEMS_PER_PAGE;
   games = games.slice(fromIndex, toIndex);
-
   const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
   const currentPage = page;
-
   const response: GamesResponse = {
     games,
     availableFilters,
--- src/services/fetchGamesAction.ts
+++ src/services/fetchGamesAction.ts
@@ -13,3 +13,18 @@ export const fetchGamesAction = async (page = 1, genre = "") => {
     console.error("Something failed while fetching the games", e);
   }
 };
+
+export const fetchGamesByIdAction = async (ids = [] as string[]) => {
+  if (ids.length === 0) return { games: [] };
+  const paramsIds = ids.join(",");
+
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/games?ids=${paramsIds}`
+    );
+    const data: Pick<GamesResponse, "games"> = await response.json();
+    return data;
+  } catch (e) {
+    console.error("Something failed while fetching the games", e);
+  }
+};
