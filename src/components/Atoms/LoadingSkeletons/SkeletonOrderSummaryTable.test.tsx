c738a2201b81ed5ed5addde505cb40bd705955f2
lucasfvera <lucasfvera@hotmail.com>
2025-07-17 01:17:56 -0300

    feat: Add skeleton component for order summary table

---
+++ src/components/Atoms/LoadingSkeletons/SkeletonOrderSummaryTable.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import { SkeletonOrderSummaryTable } from "./SkeletonOrderSummaryTable";
+
+describe("Atoms > SkeletonOrderSummaryTable", () => {
+  it("renders the skeleton with all main sections", () => {
+    // Arrange
+    render(<SkeletonOrderSummaryTable />);
+    // Act
+    const skeleton = screen.getByTestId("loading-skeleton-order-summary-table");
+
+    // Assert
+    expect(skeleton).toBeInTheDocument();
+  });
+});
---
+++ src/components/Atoms/LoadingSkeletons/SkeletonOrderSummaryTable.tsx
@@ -0,0 +1,31 @@
+export const SkeletonOrderSummaryTable = () => {
+  return (
+    <div
+      className="flex flex-col gap-8"
+      data-testid="skeleton-order-summary-table"
+    >
+      <div className="flex flex-col p-8 rounded-lg border-[#8f8f8f] border-[0.5px] gap-8 w-full">
+        <div>
+          <div className="h-7 w-40 bg-gray-300 animate-pulse rounded mb-2" />
+          <div className="h-6 w-24 bg-gray-300 animate-pulse rounded" />
+        </div>
+        <div className="flex flex-col py-5 gap-6">
+          <div className="flex flex-col gap-3">
+            {[...Array(2)].map((_, i) => (
+              <div key={i} className="flex justify-between">
+                <div className="h-6 w-32 bg-gray-300 animate-pulse rounded max-w-[60%]" />
+                <div className="h-6 w-12 bg-gray-300 animate-pulse rounded" />
+              </div>
+            ))}
+          </div>
+          <div className="border-b border-b-[#8f8f8f]" />
+          <div className="flex justify-between">
+            <div className="h-4 w-20 bg-gray-300 animate-pulse rounded" />
+            <div className="h-4 w-12 bg-gray-300 animate-pulse rounded" />
+          </div>
+        </div>
+      </div>
+      <div className="h-12 bg-gray-300 animate-pulse rounded-lg w-full" />
+    </div>
+  );
+};
